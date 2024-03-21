fe094790c2e01618f40be7b345283a631edf6a28
ndhfrayyan <74947254+ndhfrayyan@users.noreply.github.com>
2024-03-21 03:14:19 -0400

    Create B.cpp

---
+++ CF Round 935 (Div 3)/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fi first
+#define se second
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long a, b, m;;
+        cin >> a >> b >> m;
+        cout << m / a + m / b + 2 << endl;
+    }
+}
